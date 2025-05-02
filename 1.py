d039eb9a575143424d3af15edc2c70d83c5395fc
honda wataru <w.honda@miracleave.co.jp>
2025-05-02 10:43:45 +0900

    Added simple code.

---
+++ 1.py
@@ -0,0 +1,9 @@
+my_characters = ['Tabibito', 'Paimon', 'Amber', 'Gaia', 'Risa', 'Noelle',
+                'Enhi', 'Faruzan', 'Yukiaki', 'Rinnet']
+result = []
+for character in my_characters:
+    result.append(character)
+
+result.reverse()
+print(my_characters)
+print(result)
